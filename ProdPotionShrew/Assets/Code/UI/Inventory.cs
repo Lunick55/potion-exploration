3acd6a3977557cba74b5539bbae086a929916515
Karim Elzanaty <karim.elzanaty@mymail.champlain.edu>
2019-02-02 01:46:34 -0500

    Added UI test scene, and implementing a cool little UI

---
+++ ProdPotionShrew/Assets/Code/UI/Inventory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+	[SerializeField] List<Item> items;
+	[SerializeField] Transform itemParent;
+	[SerializeField] ItemSlots[] itemSlots;
+
+	private void OnValidate()
+	{
+		if (itemParent != null)
+			itemSlots = itemParent.GetComponentsInChildren<ItemSlots>();
+
+		RefreshUI();
+	}
+
+	private void RefreshUI()
+	{
+		int i = 0;
+
+		for (; i < items.Count && i < itemSlots.Length; i++)
+		{
+			itemSlots[i].item = items[i];
+		}
+		for (; i < itemSlots.Length; i++)
+		{
+			itemSlots[i].item = null;
+		}
+	}
+
+}
